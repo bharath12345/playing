e33b6b7f81380a23622f774507db62cc010ff46a
bharath12345 <bharath12345@gmail.com>
2014-01-12 20:55:57 +0530

    frontpage up!

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,16 +4,48 @@ import play.api._
 import play.api.mvc._
 import scala.collection.mutable.ListBuffer
 import play.api.Play.current
+import org.joda.time.DateTime
+import scala.collection.immutable.HashMap
+import scala.io.Source
 
 object Application extends Controller {
 
   def index = Action {
-    val posts = Play.getFile("posts")
-    val fileList = new ListBuffer[String]()
-    for(file <- posts.listFiles) {
-      fileList += file.getName
+
+    def getLine(lines: Seq[String], search: String): String = {
+      val l = lines.filter(line => line.contains(search))(0)
+      val lrhs = l.replaceAll("\"","").replaceAll(",","").trim.split(":")
+      val lhs = lrhs(0).trim;val rhs = lrhs(1).trim
+      println(s"lhs = $lhs rhs = $rhs")
+      rhs
     }
-    Ok(views.html.index(fileList.toList))
+
+    val posts = Play.getFile("posts")
+
+    val titleMap = new ListBuffer[String]
+    var dateMap = new HashMap[String, String]
+    var contentMap = new HashMap[String, Seq[String]]
+    var fileList = new HashMap[String, String]
+
+    for(file <- posts.listFiles) {
+      val lines = Source.fromFile(file).getLines().toSeq
+      val header = lines.takeWhile( line => !line.equals("}}}"))
+      val content = lines.dropWhile( line => !line.equals("}}}")).drop(1).dropWhile(line => line.length == 0)
+      val excerpt = content.takeWhile( line => line.length != 0)
+      //for(h <- excerpt) println(h + "\n\n")
+
+      val title      = getLine(header, "\"title\"")
+      titleMap += title
+
+      fileList = fileList + (title -> file.getName.replace(".md",""))
+
+      val date       = getLine(header, "\"date\"")
+      dateMap = dateMap + (title -> date)
+
+      contentMap = contentMap + (title -> excerpt)
+    }
+
+    Ok(views.html.index(titleMap.toList)(fileList)(dateMap)(contentMap))
   }
 
   def blog(id: String) = Action {
