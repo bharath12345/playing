e8ed737d8ca59693a5b4811f1db0c1454f1f7e4f
bharath12345 <bharath12345@gmail.com>
2014-01-12 22:34:36 +0530

    with sort order

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -19,13 +19,13 @@ object Application extends Controller {
       val l = lines.filter(line => line.contains(search))(0)
       val lrhs = l.replaceAll("\"","").replaceAll(",","").trim.split(":")
       val lhs = lrhs(0).trim;val rhs = lrhs(1).trim
-      println(s"lhs = $lhs rhs = $rhs")
+      //println(s"lhs = $lhs rhs = $rhs")
       rhs
     }
 
     val posts = Play.getFile("posts")
 
-    val titleMap = new ListBuffer[String]
+    var titleMap = new HashMap[Long, String]
     var dateMap = new HashMap[String, String]
     var contentMap = new HashMap[String, String]
     var fileList = new HashMap[String, String]
@@ -35,20 +35,26 @@ object Application extends Controller {
       val header = lines.takeWhile( line => !line.equals("}}}"))
       val content = lines.dropWhile( line => !line.equals("}}}")).drop(1).dropWhile(line => line.length == 0)
       val excerpt = pegdown.markdownToHtml(content.takeWhile( line => line.length != 0)(0))
-      println(excerpt)
+      //println(excerpt)
 
       val title      = getLine(header, "\"title\"")
-      titleMap += title
+      val date       = getLine(header, "\"date\"")
+
+      dateMap = dateMap + (title -> date)
+      val ymd = date.split("-")
+
+      val dt = new DateTime(ymd(2).toInt, ymd(0).toInt, ymd(1).toInt, 0, 0, 0)
+      //println("date = " + dt)
+      titleMap = titleMap + (dt.getMillis -> title)
 
       fileList = fileList + (title -> file.getName.replace(".md",""))
-
-      val date       = getLine(header, "\"date\"")
-      dateMap = dateMap + (title -> date)
-
       contentMap = contentMap + (title -> excerpt)
     }
 
-    Ok(views.html.index(titleMap.toList)(fileList)(dateMap)(contentMap))
+    val sorted = titleMap.toList.sortWith(_._1 > _._1) // sort by date
+    //println(sorted)
+
+    Ok(views.html.index(sorted.map(_._2))(fileList)(dateMap)(contentMap))
   }
 
   def blog(id: String) = Action {
