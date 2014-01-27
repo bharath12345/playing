7fa0e7bd54da3e5e91ba0b473f31f8b30b0d6392
bharath12345 <bharath12345@gmail.com>
2014-01-27 19:25:37 +0530

    slideshare + about self

---
+++ app/controllers/About.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created by bharadwaj on 27/01/14.
+ */
+object About extends Controller {
+
+  def about = Action {
+    Ok("")
+  }
+
+  def books = Action {
+    Ok("")
+  }
+
+  def current = Action {
+    Ok("")
+  }
+
+  def education = Action {
+    Ok("")
+  }
+
+  def movies = Action {
+    Ok("")
+  }
+
+  def philosophy = Action {
+    Ok("")
+  }
+
+  def travel = Action {
+    Ok("")
+  }
+
+  def work = Action {
+    Ok("")
+  }
+
+}
