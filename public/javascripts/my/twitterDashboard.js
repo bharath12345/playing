cb4bf8c16fe8d1032a7d23582a13a0a1e540ab02
bharath12345 <bharath12345@gmail.com>
2014-02-26 16:39:43 +0530

    cleaned up the high charts look and feel

--- public/javascripts/my/twitterDashboard.js
+++ public/javascripts/my/twitterDashboard.js
@@ -12,25 +12,16 @@ require(["dijit/registry", "dojox/layout/GridContainer", "dijit/TitlePane", "doj
             isAutoOrganized: true
         }, 'placeHere');
 
-        //var width = gridContainer.domNode.parentNode.offsetWidth;
-        //var height = gridContainer.domNode.offsetHeight;
-        //var styleString = "width: " + (width / 2) + "; height: " + (height / 2) + ";"
-        //console.log("style string = " + styleString)
-
         for (var i = 0; i < 4; i++) {
-            // prepare some Content for the Portlet:
             var chartId = "chart_" + i
             var innerContent = '<div class="chart" id=' + chartId + '></div>'
 
-            // create a new Portlet:
             var titlepane = new TitlePane({
                 title: 'Twitter Trends Graph',
                 content: innerContent,
                 splitter: false
-                //style:styleString
             });
 
-            // add the first Portlet to the GridContainer:
             gridContainer.addChild(titlepane, i % 2, i / 2);
         }
 
