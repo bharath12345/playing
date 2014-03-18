3c6dac363a8de7ce9ebc4e293cee5142cd83c307
bharath12345 <bharath12345@gmail.com>
2014-03-18 21:10:27 +0530

    postgres exception gone

--- modules/configuration/app/models/User.scala
+++ modules/configuration/app/models/User.scala
@@ -11,6 +11,7 @@ import com.github.tototoshi.slick.PostgresJodaSupport._
 import securesocial.core.providers.Token
 
 import org.joda.time.DateTime
+import play.api.db.DB
 
 case class User(uid: Option[Long] = None,
                 identityId: IdentityId,
@@ -180,16 +181,20 @@ class Tokens(tag: Tag) extends Table[Token](tag, "token") {
 
 trait WithDefaultSession {
 
+  import play.api.Play.current
+
   def withSession[T](block: (Session => T)) = {
     val databaseURL = play.api.Play.current.configuration.getString("db.default.url").get
     val databaseDriver = play.api.Play.current.configuration.getString("db.default.driver").get
     val databaseUser = play.api.Play.current.configuration.getString("db.default.user").getOrElse("")
     val databasePassword = play.api.Play.current.configuration.getString("db.default.password").getOrElse("")
 
-    val database = Database.forURL(url = databaseURL,
+    /*val database = Database.forURL(url = databaseURL,
       driver = databaseDriver,
       user = databaseUser,
-      password = databasePassword)
+      password = databasePassword)*/
+
+    val database = Database.forDataSource(DB.getDataSource())
 
     database withSession {
       session =>
