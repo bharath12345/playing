d133cd75b74fc3bb459dabca01ded140e07bdb2c
bharath12345 <bharath12345@gmail.com>
2014-03-19 17:45:38 +0530

    auth from all providers working. updated to sbt v0.13.1

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,16 +4,16 @@ import play.api._
 import play.api.mvc._
 import play.api.Play.current
 
-import in.bharathwrites.MyScalaEjbClient
-import in.bharathwrites.MyJavaEjbClient
+//import in.bharathwrites.MyScalaEjbClient
+//import in.bharathwrites.MyJavaEjbClient
 
 /**
  * Created by bharadwaj on 06/03/14.
  */
 object Application extends Controller {
 
-  val myjavaclient: MyJavaEjbClient = new MyJavaEjbClient
-  val myscalaclient: MyScalaEjbClient = new MyScalaEjbClient
+  //val myjavaclient: MyJavaEjbClient = new MyJavaEjbClient
+  //val myscalaclient: MyScalaEjbClient = new MyScalaEjbClient
 
   def index = Action {
     Redirect("/blog")
