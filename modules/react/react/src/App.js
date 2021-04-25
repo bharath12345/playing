efbe9dc27e476cdb3d9d0fe82b4e9a95a4ffc667
Bharadwaj Narasimha <nabharad@adobe.com>
2021-04-25 23:38:08 +0530

    added routes

--- modules/react/react/src/App.js
+++ modules/react/react/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import CustomerList from "./components/CustomerList";
 import Filter from "./components/Filter";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Customer Application</h1>
-        <CustomerList/>
+        <Router>
+            <Switch>
+                <Route path="/list" component={CustomerList} />
+                <Route path="/todo" component={CustomerList} />
+                <Route exact path="/" component={CustomerList} />
+                <Route default component={CustomerList} />
+            </Switch>
+        </Router>
     </div>
   );
 }
