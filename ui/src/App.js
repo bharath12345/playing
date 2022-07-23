b86b8b491777348cd0004256fe63bd2c14bcaffc
Bharadwaj Narasimha <bharath12345@gmail.com>
2022-07-23 12:57:40 +0530

    prettier'd

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import {Grid, View} from '@adobe/react-spectrum';
+import { Grid, View } from "@adobe/react-spectrum";
 
 const App = () => {
-  return React.createElement(Grid, {areas:['header','content','footer'], rows: ['size-500', 'auto', 'size-500'], height: "800px", gap:"size-100"}, [
-           React.createElement(View, {backgroundColor:"purple-600", gridArea:"header"}, "Bharadwaj"),
-           React.createElement(View, {}),
-           React.createElement(View, {backgroundColor:"gray-500", gridArea:"footer"}, "Powered by GitHub, Heroku, Scala, Play and React!")
-         ])
+  return React.createElement(
+    Grid,
+    {
+      areas: ["header", "content", "footer"],
+      rows: ["size-500", "auto", "size-500"],
+      height: "800px",
+      gap: "size-100",
+    },
+    [
+      React.createElement(
+        View,
+        { backgroundColor: "purple-600", gridArea: "header" },
+        "Bharadwaj"
+      ),
+      React.createElement(View, {}),
+      React.createElement(
+        View,
+        { backgroundColor: "gray-500", gridArea: "footer" },
+        "Powered by GitHub, Heroku, Scala, Play and React!"
+      ),
+    ]
+  );
 };
 
 export default App;
